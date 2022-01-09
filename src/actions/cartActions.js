28fff79121c5134de42882007159fe364c5b37ba
Brycen Harmon <brycenharmon@gmail.com>
2022-01-09 09:53:32 -0600

    Finished cart actions, state works correctly

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -2,5 +2,23 @@ import axios from 'axios'
 import { CART_ADD_ITEM } from '../constants/cartConstants'
 
 export  const addToCart = (id, qty) => async (dispatch, getState) => {
-    const {data} = await axios.get()
+    const {data} = await axios.get(`/api/products/${id}`)
+
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload:{
+            product: data._id,
+            name:data.name,
+            image:data.image,
+            price:data.price,
+            countInStock:data.countInStock,
+            qty,
+
+        }
+    })
+
+    localStorage
+
+
+
 }
