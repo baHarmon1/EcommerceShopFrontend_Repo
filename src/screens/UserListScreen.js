7170cfaa4a9b0cf3e09fdc08f9f03fb8318e0d30
Brycen Harmon <brycenharmon@gmail.com>
2022-01-12 10:19:52 -0600

    Added table of users with edit and delete button to UserListScreen

--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -4,23 +4,78 @@ import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { listUsers } from '../actions/userActions'
+import { listUsers } from "../actions/userActions";
 
 function UserListScreen() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const userList = useSelector((state) => state.userList);
+  const { loading, error, users } = userList;
+  useEffect(() => {
+    dispatch(listUsers());
+  }, [dispatch]);
 
-    const userList = useSelector(state => state.userList)
-    const{loading, error, users} = userList
-    useEffect(() => {
-        dispatch(listUsers())
-    },[dispatch])
 
-    return (
-        <div>
-            <h1>USers</h1>
-        </div>
-    )
+
+  const deleteHandler = (id => {
+      console.log('DELETE:', id)
+  })
+
+  return (
+    <div>
+      <h1>Users</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.isAdmin ? (
+                    <i className="fas fa-check" style={{ color: "green" }}></i>
+                  ) : (
+                    <i className="fas fa-check" style={{ color: "red" }}></i>
+                  )}
+                </td>
+
+                <td>
+                  <LinkContainer to={`/admin/users/${user._id}`}>
+                    <Button variant="light" className="btn-sm">
+                      <i className="fas fa-edit"></i>
+                    </Button>
+                  </LinkContainer>
+
+                  <Button
+                    variant="danger"
+                    className="btn-sm"
+                    onClick={() => deleteHandler(user._id)}
+                  >
+                    <i className="fas fa-trash"></i>
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </div>
+  );
 }
 
-export default UserListScreen
+export default UserListScreen;
