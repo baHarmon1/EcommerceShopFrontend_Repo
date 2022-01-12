59329698a5b86e132027b96339ae9d34643cef95
Brycen Harmon <brycenharmon@gmail.com>
2022-01-12 17:15:18 -0600

    added error handler for image uploading

--- src/screens/ProductEditScreen.js
+++ src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,7 +7,7 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import FormContainer from "../components/FormContainer";
 import { listProductDetails, updateProduct } from "../actions/productActions";
-import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
+import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
 
 function ProductEditScreen() {
   const dispatch = useDispatch();
@@ -22,56 +23,89 @@ function ProductEditScreen() {
   const [category, setCategory] = useState("");
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
+  const [uploading, setUploading] = useState(false);
 
   const productDetails = useSelector((state) => state.productDetails);
   const { error, loading, product } = productDetails;
 
   const productUpdate = useSelector((state) => state.productUpdate);
-  const { error: errorUpdate, loading: loadingUpdate, success: successUpdate } = productUpdate;
+  const {
+    error: errorUpdate,
+    loading: loadingUpdate,
+    success: successUpdate,
+  } = productUpdate;
 
   useEffect(() => {
-
-    if(successUpdate){
-        dispatch({type: PRODUCT_UPDATE_RESET})
-        navigate('/admin/productlist')
-    }else{
-        if (!product.name || product._id !== Number(productId)) {
-          dispatch(listProductDetails(productId));
-        } else {
-          setName(product.name);
-          setPrice(product.price);
-          setImage(product.image);
-          setBrand(product.brand);
-          setCategory(product.category);
-          setCountInStock(product.countInStock);
-          setDescription(product.description);
-        }
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET });
+      navigate("/admin/productlist");
+    } else {
+      if (!product.name || product._id !== Number(productId)) {
+        dispatch(listProductDetails(productId));
+      } else {
+        setName(product.name);
+        setPrice(product.price);
+        setImage(product.image);
+        setBrand(product.brand);
+        setCategory(product.category);
+        setCountInStock(product.countInStock);
+        setDescription(product.description);
+      }
     }
-
   }, [product, productId, navigate, dispatch, successUpdate]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(updateProduct({
-        _id:productId,
+    dispatch(
+      updateProduct({
+        _id: productId,
         name,
         price,
         image,
         brand,
         category,
         countInStock,
-        description
-    }))
+        description,
+      })
+    );
   };
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0]
+    const formData = new FormData()
+
+    formData.append('image', file)
+    formData.append('product_id', productId)
+
+    setUploading(true)
+
+    try{
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+
+      const {data} = await axios.post('/api/products/upload/', formData, config)
+
+      setImage(data)
+      setUploading(false)
+
+
+    }catch(error){
+      setUploading(false)
+    }
+
+  };
+
   return (
     <div>
       <Link to="/admin/productlist">Go Back</Link>
 
       <FormContainer>
         <h1>Edit Product</h1>
-        {loadingUpdate && <Loader/>}
+        {loadingUpdate && <Loader />}
         {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
-
 
         {loading ? (
           <Loader />
@@ -107,6 +141,16 @@ function ProductEditScreen() {
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
+
+              <Form.Group
+                controlId="image-file"
+                className="mb-3"
+                onChange={uploadFileHandler}
+              >
+                <Form.Label>Choose File</Form.Label>
+                <Form.Control type="file" />
+              </Form.Group>
+              {uploading && <Loader/>}
             </Form.Group>
 
             <Form.Group controlId="brand">
